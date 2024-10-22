e14e51072cc656fd56b1e15067ed72799fd29c01
yaniv-golan <yaniv@golan.name>
2024-10-22 18:25:40 +0300

    Restored _static directory

--- docs/conf.py
+++ docs/conf.py
@@ -49,4 +49,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-# html_static_path = ['_static']  # Commented out if _static directory doesn't exist
+html_static_path = ['_static']  # Commented out if _static directory doesn't exist
