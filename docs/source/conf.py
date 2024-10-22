f901c06b7df64181b2f831e01338cf3dd9a7b8bb
yaniv-golan <yaniv@golan.name>
2024-10-22 18:58:32 +0300

    Updated docs to use sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+import sphinx_rtd_theme
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -16,28 +20,27 @@ release = '0.1.0'
 
 extensions = [    
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-import os
-import sys
+html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
 
 # Add the project root to the Python path
 sys.path.insert(0, os.path.abspath('../..'))
 
 # Debugging information
+print("Python version:", sys.version)
+print(f"sphinx_rtd_theme version: {sphinx_rtd_theme.__version__}")
 print("Current working directory:", os.getcwd())
+print("Contents of current directory:", os.listdir())
 print("Python path:", sys.path)
 
 try:
